6405f27838ab18bf31d8c2697db8c1598e0de927
khalil kml <khalilkml123@gmail.com>
2023-11-29 20:58:14 +0100

    fix : payment with paypal

--- app/src/main/java/com/example/payway/activities_and_fragments/fragments/My_cart.java
+++ app/src/main/java/com/example/payway/activities_and_fragments/fragments/My_cart.java
@@ -8,9 +8,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.OnBackPressedCallback;
 import androidx.fragment.app.Fragment;
@@ -25,6 +25,22 @@ import com.example.payway.activities_and_fragments.Activities.MainActivity;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.paypal.checkout.approve.Approval;
+import com.paypal.checkout.approve.OnApprove;
+import com.paypal.checkout.createorder.CreateOrder;
+import com.paypal.checkout.createorder.CreateOrderActions;
+import com.paypal.checkout.createorder.CurrencyCode;
+import com.paypal.checkout.createorder.OrderIntent;
+import com.paypal.checkout.createorder.UserAction;
+import com.paypal.checkout.order.Amount;
+import com.paypal.checkout.order.AppContext;
+import com.paypal.checkout.order.CaptureOrderResult;
+import com.paypal.checkout.order.OnCaptureComplete;
+import com.paypal.checkout.order.OrderRequest;
+import com.paypal.checkout.order.PurchaseUnit;
+import com.paypal.checkout.paymentbutton.PaymentButtonContainer;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -39,6 +55,12 @@ public class My_cart extends Fragment  {
     private ProductCardManager productCardManager;
 
     private List<Product> productList;
+    //    Button bynow;
+    AtomicReference<Double> totalDouble;
+
+    //paypal variables
+    private static final String TAG = "MyTag";
+    PaymentButtonContainer paymentButtonContainer;
 
 
     public My_cart() {
@@ -68,7 +90,9 @@ public class My_cart extends Fragment  {
         ImageView back = view.findViewById(R.id.back);
         RecyclerView recyclerView = view.findViewById(R.id.recycler_view2);
         TextView total = view.findViewById(R.id.Totale);
-        Button bynow = view.findViewById(R.id.Bynow);
+        paymentButtonContainer=view.findViewById(R.id.payment_button_container);
+//        paymentButtonContainer.setAlpha(0.9f);
+//        bynow = view.findViewById(R.id.Bynow);
 
         //2 cards in row
         recyclerView.setLayoutManager(new GridLayoutManager(getContext(),2));
@@ -79,7 +103,7 @@ public class My_cart extends Fragment  {
                 .document(currentUserId())
                 .collection("productList");
 
-        AtomicReference<Double> totalDouble = new AtomicReference<>(0.0); // Initialize as AtomicReference
+        totalDouble = new AtomicReference<>(0.0); // Initialize as AtomicReference
         // Retrieve data from Firestore
         productListRef.get().addOnCompleteListener(task -> {
             if (task.isSuccessful()) {
@@ -108,13 +132,7 @@ public class My_cart extends Fragment  {
 
 
 
-        back.setOnClickListener(v -> {
-            if (getActivity() instanceof MainActivity) {
-                MainActivity mainActivity = (MainActivity) getActivity();
-                mainActivity.cartbackpress();
-            }
-        });
-        // Handle back press of the phone
+
         requireActivity().getOnBackPressedDispatcher().addCallback(getViewLifecycleOwner(), new OnBackPressedCallback(true) {
             @Override
             public void handleOnBackPressed() {
@@ -125,12 +143,15 @@ public class My_cart extends Fragment  {
             }
         });
 
-        bynow.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
+        // Handle back press of the phone
+        back.setOnClickListener(v -> {
+            if (getActivity() instanceof MainActivity) {
+                MainActivity mainActivity = (MainActivity) getActivity();
+                mainActivity.cartbackpress();
             }
         });
+
+        initiatePayPalPayment();
 
         return view;
     }
@@ -156,6 +177,51 @@ public class My_cart extends Fragment  {
         MainActivity mainActivity = (MainActivity) requireActivity();
         // Hide the bottom app bar
         mainActivity.showBottomAppBar();
+    }
+
+    private void initiatePayPalPayment(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            paymentButtonContainer.setup(
+                new CreateOrder() {
+                    @Override
+                    public void create(@NotNull CreateOrderActions createOrderActions) {
+                        Log.d(TAG, "create: ");
+                        ArrayList<PurchaseUnit> purchaseUnits = new ArrayList<>();
+                        purchaseUnits.add(
+                                new PurchaseUnit.Builder()
+                                        .amount(
+                                                new Amount.Builder()
+                                                        .currencyCode(CurrencyCode.USD)
+                                                        .value(String.valueOf(totalDouble))
+                                                        .build()
+                                        )
+                                        .build()
+                        );
+                        OrderRequest order = new OrderRequest(
+                                OrderIntent.CAPTURE,
+                                new AppContext.Builder()
+                                        .userAction(UserAction.PAY_NOW)
+                                        .build(),
+                                purchaseUnits
+                        );
+                        createOrderActions.create(order, (CreateOrderActions.OnOrderCreated) null);
+                    }
+                },
+
+                new OnApprove() {
+                    @Override
+                    public void onApprove(@NotNull Approval approval) {
+                        approval.getOrderActions().capture(new OnCaptureComplete() {
+                            @Override
+                            public void onCaptureComplete(@NotNull CaptureOrderResult result) {
+                                Log.d(TAG, String.format("CaptureOrderResult: %s", result));
+                                Toast.makeText(requireContext(), "Successful", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                }
+            );
+        }
     }
 
 }
